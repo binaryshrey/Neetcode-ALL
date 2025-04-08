899d4bb1322c8d366531899b29ce00a8d8ebed4d
Shreyansh Saurabh <shreyansh.saurabh01@gmail.com>
2025-04-08 11:22:42 +0530

    concat.py

---
+++ 002_concat.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/concatenation-of-array/
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.extend(nums)
+        return nums
+        
